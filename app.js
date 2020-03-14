51974c342b0faf909afe7fdfdc7ec37b53014900
“Stefan <andreistefanciprian@gmail.com>
2020-03-14 20:57:58 +0000

    update ver

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log("Node app starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit v1 running on: " + os.hostname() + "\n");
+  response.end("You've hit v0.1.11 running on: " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
