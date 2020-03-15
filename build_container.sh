806924229867f8aec54af7486ec62ae9b806108d
“Stefan <andreistefanciprian@gmail.com>
2020-03-15 15:54:40 +0000

    update script

--- build_container.sh
+++ build_container.sh
@@ -1,17 +1,25 @@
-# This is a script to quickly build this container fot test/debug purposes
+# This is a script to quickly build and push this container for test/debug purposes
 
 # variables
 CONTAINER_NAME=nodejs-app
-TAG="v01.20"
+TAG="v01.11"
+REPO_CONTAINER_NAME_TAG="andreistefanciprian/${CONTAINER_NAME}:${TAG}"
 
-# remove container if exists
-if [ `docker ps --all --quiet --filter=name=$CONTAINER_NAME` ]; then docker container rm -f $CONTAINER_NAME && echo "Deleted container $CONTAINER_NAME"; else echo "Container $CONTAINER_NAME not present/running" ; fi
+echo -e "\nRemove container if exists..."
+container_id=$(docker ps -aqf "name=^${CONTAINER_NAME}$")
+[ -z $container_id ] || docker container rm -f $container_id
 
 # build image
-docker image build -t nodejs-app:${TAG} .
+echo -e "\nBuild image..."
+docker image build -t $CONTAINER_NAME:$TAG .
 
 # run container
-docker run -d -p 8080:8080 --name $CONTAINER_NAME nodejs-app:${TAG}
+echo -e "\nRun container..."
+docker run -d -p 8080:8080 --name $CONTAINER_NAME $CONTAINER_NAME:$TAG
 
-# list container
+# list containers
+echo -e "\nList containers..."
 docker container ls
+
+# specify push as positional param if you want to push this image to dockerhub
+[ "$1" == "push" ] && ( docker image tag $CONTAINER_NAME:$TAG $REPO_CONTAINER_NAME_TAG && docker image push $REPO_CONTAINER_NAME_TAG)
